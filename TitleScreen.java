c681ddd37cdf204b443eb0195a4ca8fd3c245a4b
Robert Jeon <60145948+Error4042312@users.noreply.github.com>
2021-04-16 11:38:45 -0400

    Create TitleScreen.java

---
+++ TitleScreen.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+import java.awt.Container; 
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class TitleScreen {
+
+  JFrame.window;
+  Container con;
+  JPanel titleNamePanel;
+  JLabel titleNameLabel; 
+
+  public static void main(String[] args) {
+    new TitleScreen(); 
+  } 
+
+  public TitleScreen
+
+
+}
