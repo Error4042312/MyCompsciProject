27089522f3d42426f33d9b2625b4ba403c3f96db
Robert Jeon <60145948+Error4042312@users.noreply.github.com>
2021-04-28 11:36:44 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,29 @@
-import javax.swing.JFrame;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+//package com.tutorialspoint.gui;
+
+//import javax.swing.JFrame;
+//import javax.swing.ImageIcon;
+//import javax.swing.JLabel;
+
+//import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 class Main {
   public static void main(String args[]) {
+    JFrame s = new JFrame();
+    //ImageIcon icon = new ImageIcon("imgs/DumplingSoup.jpg");
+   // JLabel label = new JLabel(icon);
+    //frame.add(label);
+    JButton okButton = new JButton("OK");
+    okButton.setBounds(80, 100, 250, 40);
+    s.add(okButton);
+    s.setSize(400, 500);
+    s.setLayout(null);
+   
+    //s.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //s.pack(); 
+    s.setVisible(true);
 
-    JFrame frame = new JFrame();
-    ImageIcon icon = new ImageIcon("DumplingSoup.java");
-    JLabel label = new JLabel(icon);
-    frame.add(label);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.pack(); 
-    frame.setVisible(true);
+    //new TitleScreen();
   }
 }
