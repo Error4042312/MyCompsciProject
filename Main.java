cbd719b0133f85da0beaa7e14523e2f3934e1e30
Robert Jeon <60145948+Error4042312@users.noreply.github.com>
2021-05-14 00:26:24 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
- //package com.tutorialspoint.gui;
+//Important Info: User may have to drag some windows around in program to see the ingredients for the food properly. 
 
-//import javax.swing.JFrame;
-//import javax.swing.ImageIcon;
-//import javax.swing.JLabel;
-
-//import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
@@ -18,7 +13,10 @@ class Main {
     JFrame thirdRegion4 = new JFrame();
 
     //frame containing ingredients
+    JFrame thirdRegion1Ing = new JFrame();
     JFrame thirdRegion2Ing = new JFrame();
+    JFrame thirdRegion3Ing = new JFrame();
+    JFrame thirdRegion4Ing = new JFrame();
 
       
     //new button  
@@ -28,6 +26,13 @@ class Main {
     JButton region2 = new JButton("United States");
     JButton region3 = new JButton("Mexico");
     JButton region4 = new JButton("Canada");
+
+    //buttons for ingredients
+    JButton ingredients1 = new JButton("Ingredients: 8 Korean dumplings, 4 1/2 cups Korean stock, 1 Tbsp regular soy sauce, 1/2 tsp minced garlic, 2 eggs (beaten), 1 tsp sesame oil, 1  to 2 stalks green onion, thinly sliced, (optional) 2 eggs, egg white and yolk separated, (optional) dried seaweed (gim / nori), thinly sliced, (optional) fine sea salt, to taste, (optional) black pepper, to taste");
+    JButton ingredients2 = new JButton("Ingredients: 2 pounds freshly ground chuck, (at least 80% lean), 1 tablespoon onion powder, 1 teaspoon salt, 1 teaspoon freshly ground black pepper, 2 slices deli-counter American cheese, 1 large burger bun");
+    JButton ingredients3 = new JButton("Ingredients: 1 lb. 90% to 93% lean ground beef, 1 Tablespoon chili powder, 1 teaspoon ground cumin, 3/4 teaspoon salt, 1/2 teaspoon dried oregano, 1/2 teaspoon garlic powder, 1/4 teaspoon ground black pepper, 1/2 cup tomato sauce, 1/4 cup water, 12 taco shells - either hard shells or small 6-inch soft flour tortillas will work, (Optional) Taco Toppings: shredded cheese shredded lettuce, chopped tomatoes, diced red onion, taco sauce, sour cream, guacamole, etc.");
+    JButton ingredients4 = new JButton("Ingredients: 3 Tbsp cornstarch, 2 Tbsp water, 6 Tbsp unsalted butter, 1/4 cup unbleached all purpose flour, 20 oz beef broth, 10 oz chicken broth,Pepper, 2 lbs Russet potatoes (3-4 medium potatoes), Peanut or other frying oil,1 - 1 1/2 cups white cheddar cheese curds");
+    
    
     //setting up the dimensions for the buttons
     titleToNext.setBounds(105, 350, 250, 40);
@@ -36,6 +41,10 @@ class Main {
     region2.setBounds(350, 100, 200, 40);
     region3.setBounds(40, 250, 200, 40);
     region4.setBounds(350, 250, 200, 40);
+    ingredients1.setBounds(125, 300, 550, 400);
+    ingredients2.setBounds(125, 300, 550, 400);
+    ingredients3.setBounds(125, 300, 550, 400);
+    ingredients4.setBounds(125, 300, 550, 400);
     first.setVisible(true);
     second.setVisible(false);
     thirdRegion1.setVisible(false);
@@ -59,6 +68,7 @@ class Main {
       public void actionPerformed(ActionEvent e) {
         second.setVisible(false);
         thirdRegion1.setVisible(true);
+        thirdRegion1Ing.setVisible(true);
       }
     });
 
@@ -67,6 +77,7 @@ class Main {
       public void actionPerformed(ActionEvent e) {
         second.setVisible(false);
         thirdRegion2.setVisible(true);
+        thirdRegion2Ing.setVisible(true);
       }
     });
 
@@ -75,6 +86,7 @@ class Main {
       public void actionPerformed(ActionEvent e) {
         second.setVisible(false);
         thirdRegion3.setVisible(true);
+        thirdRegion3Ing.setVisible(true);
       }
     });
 
@@ -83,6 +95,7 @@ class Main {
       public void actionPerformed(ActionEvent e) {
         second.setVisible(false);
         thirdRegion4.setVisible(true);
+        thirdRegion4Ing.setVisible(true);
       }
     });
  
@@ -98,6 +111,22 @@ class Main {
     second.setSize(600, 500);
     second.setLayout(null);
 
+    thirdRegion1Ing.add(ingredients1);
+    thirdRegion1Ing.pack();
+    thirdRegion1Ing.setLayout(null);
+
+    thirdRegion2Ing.add(ingredients2);
+    thirdRegion2Ing.pack();
+    thirdRegion2Ing.setLayout(null);
+
+    thirdRegion3Ing.add(ingredients3);
+    thirdRegion3Ing.pack();
+    thirdRegion3Ing.setLayout(null);
+
+    thirdRegion4Ing.add(ingredients4);
+    thirdRegion4Ing.pack();
+    thirdRegion4Ing.setLayout(null);
+
     ImageIcon icon = new ImageIcon("imgs/DumplingSoup.jpg");
     JLabel label = new JLabel(icon);
     thirdRegion1.add(label);
@@ -111,37 +140,18 @@ class Main {
     thirdRegion2.pack();
     thirdRegion2.setLayout(null);
 
-    ImageIcon icon3 = new ImageIcon("imgs/Tacos.jpg");
+    ImageIcon icon3 = new ImageIcon("imgs/TacosFinal.jpg");
     JLabel label3 = new JLabel(icon3);
     thirdRegion3.add(label3);
     //thirdRegion3.setSize(600, 500);
     thirdRegion3.pack();
     thirdRegion3.setLayout(null);
 
-    ImageIcon icon4 = new ImageIcon("imgs/Poutine.jpg");
+    ImageIcon icon4 = new ImageIcon("imgs/PoutineFinal.jpg");
     JLabel label4 = new JLabel(icon4);
     thirdRegion4.add(label4);
     //thirdRegion4.setSize(600, 500);
     thirdRegion4.pack();
     thirdRegion4.setLayout(null);
-   
-   /* ImageIcon icon = new ImageIcon("imgs/DumplingSoup.jpg");
-    ImageIcon icon2 = new ImageIcon("imgs/Cheeseburger.jpg");
-    JLabel label = new JLabel(icon);
-    JLabel label2 = new JLabel(icon2);
-    c.add(label);
-    f.add(label2); */
-
-
-   //private class ButtonClickListener implements ActionListener{
-      //public void actionPerformed(ActionEvent e) {
-       //  String command = e.getActionCommand();  
-         
-       //  if( command.equals( "OK" ))  {
-        //    statusLabel.setText("Ok Button clicked.");
-   // f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-   // f.pack(); 
-
-   // new TitleScreen();
   }
 }
